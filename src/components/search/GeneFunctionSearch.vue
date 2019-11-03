c2edb9815b68e5aa29a298013840c77edbdf92db
Ryanhui <ryanhui1996@gmail.com>
2019-11-03 21:00:16 +0800

    domin

--- src/components/search/GeneFunctionSearch.vue
+++ src/components/search/GeneFunctionSearch.vue
@@ -105,12 +105,27 @@
     </div>
     <div>
       <h3>8. Protein domain</h3>
+      <div class="canvas">
+        <canvas ref="canvas" width="900px" height="400px" >您的浏览器暂不支持canvas</canvas>
+      </div>
+      <div ref="domin_detail" id="domin_detail">
+        <p>Domin id: {{domin_detail_item.domin_id}}</p>
+        <p>Position: {{domin_detail_item.start}} to {{domin_detail_item.end}}</p>
+        <p>E-value: {{domin_detail_item.e_value}}</p>
+        <p>Gene length: {{domin_detail_item.gene_length}}</p>
+        <p>Annotation: {{domin_detail_item.annotation}}</p>
+        <p style="word-break:break-word;">pep sequence: {{domin_detail_item.pep}}</p>
+        <p style="word-break:break-word;">cds sequence: {{domin_detail_item.cds}}</p>
+      </div>
     </div>
     <div>
       <h3>9. KEGG Pathway</h3>
     </div>
     <div>
       <h3>10. Gene Ontology</h3>
+    </div>
+    <div>
+      <h3>11. Orthologous in Rattans</h3>
     </div>
   </div>
 </template>
@@ -125,6 +140,8 @@ export default {
     return {
       gfs_tableData: [],
       function_module_tableData: [],
+      dominData: [],
+      domin_detail_item: {},
     }
   },
   methods: {
@@ -142,11 +159,102 @@ export default {
       this.axios.get(`http://rattan.bamboogdb.org/php/search_fun_module.php?keyword=${this.$route.params.gene}&species=${this.$route.params.species}`).then((response)=>{
         this.function_module_tableData = response.data || [];
       })
+    },
+    domin_submit() {
+      this.axios.get(`http://rattan.bamboogdb.org/php/search_domin.php?keyword=${this.$route.params.gene}&species=${this.$route.params.species}`).then((response)=>{
+        console.log(response.data);
+        this.dominData = response.data;
+        if(response.data.length > 0) {
+          this.canvasDraw();
+        }
+      })
+    },
+    // draw picture
+    canvasDraw() {
+      let canvas = this.$refs.canvas;
+      let ctx = canvas.getContext("2d");
+
+      // 获取鼠标相对cavas位置
+	    function getMousePosCanvas(canvas, evt) {
+	      var rect = canvas.getBoundingClientRect(); 
+	      return { 
+	        x: evt.clientX - rect.left * (canvas.width / rect.width),
+	        y: evt.clientY - rect.top * (canvas.height / rect.height)
+	      }
+      }
+
+      // 获取鼠标相对于屏幕位置
+      function getMousePos(event) {
+        var e = event || window.event;
+        return {'x':e.clientX,'y':e.clientY}
+      }
+
+      let maxLength = 0;
+      
+      ctx.font = '18px Arial';
+      ctx.fillStyle = 'rgba(50, 50, 50, 1)';
+      ctx.fillText(this.$route.params.gene, 10, 20);
+
+      // 画gene线
+      this.dominData.forEach((item) => {
+        let newLength = Number(item.end);
+        if( newLength > maxLength) {
+          maxLength = newLength;
+        }
+      });
+      // 重设画布大小
+      if(maxLength >= 900) {
+        canvas.width = maxLength;
+      }
+      ctx.fillStyle = 'rgba(50, 50, 50, 0.5)';
+      ctx.fillRect(0, 200, maxLength + 100, 3);
+
+      // 画比例尺
+      ctx.font = '12px serif';
+      ctx.fillStyle = 'rgba(50, 50, 50, 0.8)';
+      for(let i = 0; i <= maxLength + 200; i+=100) {
+        ctx.fillRect(i, 255, 1, 3);
+        ctx.fillText(i, i, 270);
+      }
+      ctx.fillRect(0, 257, maxLength + 200, 1);
+
+      // 画domin
+      ctx.fillStyle = 'rgba(50, 50, 50, 0.7)';
+      this.dominData.forEach(item => {
+        ctx.fillRect(Number(item.start), 186, Number(item.domin_length), 30);
+      })
+
+      //添加事件
+      let that = this;
+      ctx.save();
+      canvas.addEventListener("mousemove", function(event) {
+        let mousePosCanvas = getMousePosCanvas(canvas, event);
+        let pageX = mousePosCanvas.x;
+        let pageY = mousePosCanvas.y;
+
+        let mousePos = getMousePos();
+        
+        //that.$refs.domin_detail.style.display = 'none';
+        
+        that.dominData.forEach(item => {
+          if(pageX >= Number(item.start) && pageX <= Number(item.end) && pageY >= 190 && pageY <= 220){
+            // console.log(item);
+            that.domin_detail_item = item;
+            that.$refs.domin_detail.style.display = 'block';
+            that.$refs.domin_detail.style.left = mousePos.x + 10 + 'px';
+            that.$refs.domin_detail.style.top = mousePos.y + 20 + 'px';
+          }
+        })
+      });
+      canvas.addEventListener("click", function(event) {
+        that.$refs.domin_detail.style.display = 'none';
+      });
     }
   },
   mounted: function() {
     this.gfs_submit();
     this.function_module_submit();
+    this.domin_submit();
   }
 }
 
@@ -162,6 +270,30 @@ export default {
   }
   .link {
     color: rgb(11, 146, 63);
+
+  }
+  .canvas {
+    padding: 16px; 
+    border: 1px solid black; 
+    width: 932px; 
+    height: 432px;
+    margin: 0 auto;
+    overflow-x: scroll;
+  }
+  #domin_detail {
+    position: absolute;
+    z-index: 99;
+    width: 400px;
+    min-height: 300px;
+    padding: 24px;
+    text-align: left;
+    top: 300px;
+    left: 400px;
+    background: rgb(245, 245, 245);
+    display: none;
+  }
+  #domin_detail p {
+    padding: 4px 0;
   }
 </style>
 
